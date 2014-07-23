773d34b7998845e00952a270ad4b40441f1c034c
idan <idanamit4@gmail.com>
2014-07-23 23:34:20 +0300

    Added logic for the success of the post request

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,12 @@
 function Cart($scope, $http)
 {
     $scope.checkout = function(){
-        $http.post('http://localhost:9000/sale/create', $scope.cart).success(function(data, status){
+        $http.post('http://localhost:9000/sale/create', $scope.cart.products).success(function(data, status){
            $scope.sale = data;
+
+            $scope.cart.products.splice(0, $scope.cart.products.length);
+            $scope.cart.currentPrice = 0;
+            $scope.cart.checkoutCompleted = true;
         });
-    }
+    };
 }
